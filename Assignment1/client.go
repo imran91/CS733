4bbd10a3d6df47bfec5fe3b8d089c889d7360f97
imran <pathanimrankhan619@gmail.com>
2016-01-19 11:10:33 +0530

    basic parsing is done

--- Assignment1/client.go
+++ Assignment1/client.go
@@ -1,23 +1,47 @@
-package main 
-import "net" 
-import "fmt" 
-import "bufio" 
-import "os" 
+package main
 
-func main() {   
-// connect to this socket   
-conn, err := net.Dial("tcp", "127.0.0.1:8081")   
-if err!= nil {
-  	fmt.Println("Error connecting")  	
-  }
-for {     // read in input from stdin     
-		reader := bufio.NewReader(os.Stdin)     
-		fmt.Print("Text to send: ")     
-		text, _ := reader.ReadString('\n')     
-		// send to socket     
-		fmt.Fprintf(conn, text + "\n")     
-		// listen for reply     
-		message, _ := bufio.NewReader(conn).ReadString('\n')     
-		fmt.Print("Message from server: "+message)   
-	} 
-} 
+import (
+	//"bufio"
+	"fmt"
+	"net"
+	/*"strconv"
+	"strings"
+	"os"
+	"testing"*/)
+
+func main() {
+	name := "hi.txt"
+	contents := "byeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	exptime := 300
+
+	// connect to this socket
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println("Error connecting")
+	}
+
+	//scanner := bufio.NewScanner(conn)
+
+	fmt.Fprintf(conn, "write %v %v %v\r\n%v\r\n", name, len(contents), exptime, contents)
+	/*scanner.Scan() // read first line
+	  resp := scanner.Text() // extract the text from the buffer
+	  arr := strings.Split(resp, " ") // split into OK and <version>
+	  //expect(t, arr[0], "OK")
+	  ver, err := strconv.Atoi(arr[1]) // parse version as number
+	  if err != nil {
+	  		fmt.Println("Non-numeric version found")
+	  	}
+	  	version := int64(ver)
+	  fmt.Println("%v %v ",arr[0],version)
+	  for {     // read in input from stdin
+	  		reader := bufio.NewReader(os.Stdin)
+	  		fmt.Print("Text to send: ")
+	  		text, _ := reader.ReadString('\n')
+	  		// send to socket
+	  		fmt.Fprintf(conn, text + "\n")
+	  		// listen for reply
+	  		message, _ := bufio.NewReader(conn).ReadString('\n')
+	  		fmt.Print("Message from server: "+message)
+	  	}
+	*/
+}
--- Assignment1/server.go
+++ Assignment1/server.go
@@ -1,52 +1,174 @@
 package main
 
-import ( 
-		 "net"
-		 "fmt" 
-     "bufio"
-     "strings"
-		 )
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"io"
+)
 
-
-func handleConnection(conn net.Conn){
-
-  for{
-    // will listen for message to process ending in newline (\n)     
-    message, _ := bufio.NewReader(conn).ReadString('\n')     
-    // output message received     
-    fmt.Print("Message Received:", string(message))     
-    // sample process for string received     
-    newmessage := strings.ToUpper(message)    
-     // send new string back to client     
-    conn.Write([]byte(newmessage + "\n"))   
-  } 
+type command_meta struct {
+	operation string
+	filename  string
+	numbytes  int
+	expiry    int64
+	version   int
+  data string
 }
-var ln net.Listener
+
+//parse the command and return it into command_meta object
+func parser(cmd string) command_meta {
+	cobj := command_meta{"", "", 0, 0, 0,""}
+	command_split := strings.Split(cmd, " ")
+	cobj.operation = command_split[0]
+
+	switch command_split[0] {
+	case "write":
+		cobj.filename = command_split[1]
+		nbytes, err1 := strconv.Atoi(command_split[2])
+		if err1 != nil {
+			log.Println(err1)
+			break
+		}
+		cobj.numbytes = nbytes
+		exp, err2 := strconv.Atoi(command_split[3])
+		if err2 != nil {
+			log.Println(err2)
+			break
+		}
+		cobj.expiry = int64(exp)
+		return cobj
+
+	case "read":
+		cobj.filename = command_split[1]
+		return cobj
+	case "cas":
+		cobj.filename = command_split[1]
+		ver, err1 := strconv.Atoi(command_split[2])
+		if err1 != nil {
+			log.Println(err1)
+			break
+		}
+		cobj.version = ver
+		nbytes, err2 := strconv.Atoi(command_split[3])
+		if err2 != nil {
+			log.Println(err2)
+			break
+		}
+		cobj.numbytes = nbytes
+		if len(command_split) == 5 {
+			exp, err3 := strconv.Atoi(command_split[4])
+			if err3 != nil {
+				log.Println(err3)
+				break
+			}
+			cobj.expiry = int64(exp)
+		}
+		return cobj
+	case "delete":
+		cobj.filename = command_split[1]
+		return cobj
+	}
+	return cobj
+}
+
+func handleConnection(conn net.Conn) {
+	addr := conn.RemoteAddr()
+	log.Println(addr, "connected.")
+	reader := bufio.NewReader(conn)
+	for {
+
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			//log.Println("ERROR reading:")
+			continue
+		}
+		message = strings.TrimRight(message, "\r\n")
+		parsed_comm := parser(message)
+    switch parsed_comm.operation {
+    case "write":
+        buffer := make([]byte, parsed_comm.numbytes)
+        _, err1 := io.ReadFull(reader, buffer)
+        if (err1) != nil {
+          //Read error
+          log.Println("ERROR reading data:", addr, err1)
+          break
+        }
+
+        extra_data, err2 := reader.ReadString('\n')
+        if (err2) != nil {
+          //Read error
+          log.Println("ERROR reading post-data:", addr, err2)
+          break
+        }
+        parsed_comm.data = string(buffer)
+        if (strings.TrimRight(extra_data,"\r\n") != "") || (len(parsed_comm.data) != parsed_comm.numbytes) {
+          log.Println("ERROR IN CMD")
+          continue
+        }
+        break;
+    }
+
+    
+    fmt.Println("filename:", parsed_comm.filename)
+    fmt.Println(" numbytes:", parsed_comm.numbytes)
+    fmt.Println(" expiry: ", parsed_comm.expiry)
+    fmt.Println(" version: ", parsed_comm.version)
+    fmt.Println(" data: ", parsed_comm.data)
+		/*cmd, e := parser(message)
+		  if e != nil {
+		    //write(writer, addr, e.Error())
+		    log.Println("ERROR parsing:", addr, e)
+		    } else {
+
+		    }*/
+		// output message received
+
+		// sample process for string received
+		//newmessage := strings.ToUpper(message)
+		// send new string back to client
+		//conn.Write([]byte(newmessage + "\n"))
+	}
+
+	conn.Close() //used for clean up actions
+}
+
+func serverMain() {
+	var ln net.Listener
+	var list_err error
+	fmt.Println("Starting server...")
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for {
+		ln, list_err = net.ListenTCP("tcp", tcpAddr)
+		if list_err != nil {
+			log.Println(err)
+		} else {
+			break
+		}
+	}
+
+	defer ln.Close()
+
+	// run loop forever (or until ctrl-c)
+	for {
+		// accept connection on port
+		conn, con_err := ln.Accept()
+		if con_err != nil {
+			fmt.Println("Hello", con_err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
 func main() {
-  
-
-
-fmt.Println("Starting server...")
-
-  // listen on all interfaces
-//for {
-    ln, list_err := net.Listen("tcp", ":8081")
-    if list_err!= nil {
-            fmt.Println(list_err)
-    } /*else {
-      		  	break
-    	   	  }
-  }
-*/
-// run loop forever (or until ctrl-c)   
-for {     
-	// accept connection on port
-  conn, con_err := ln.Accept()
-  if con_err!= nil {
-  		fmt.Println(con_err)
-      continue
-  	}
-  go handleConnection(conn)
-}
-
+	serverMain()
 }
